24e93389aeb2ac4c9510b0894cfe637ea3cc218d
FenX999 <gabriel.tournier@gmail.com>
2022-04-30 13:46:20 +0200

    first commit

---
+++ Rust_Blockchain/blockchain.rs
@@ -0,0 +1,157 @@
+use serde_derive::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use std::fmt::Write;
+
+use chrono::prelude::*;
+
+#[derive(Debug, Clone, Serialize)]
+struct Transaction{
+    sender: String,
+    receiver: String,
+    amount: f32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32,
+    pre_hash: String,
+    merkle: String,
+    difficulty: u32,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Block{
+    header: Blockheader,
+    count: u32,
+    transaction: Vec<Transaction>,
+}
+pub struct Chain{
+    chain: Vec<Block>,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String,
+    difficulty: u32,
+}
+impl Chain{
+    pub fn new(miner_addr: String, difficulty: u32)-> Chain{
+        let mut chain = Chain{
+            chain: Vec<new()>,
+            curr_trans: Vec<new()>,
+            difficulty,
+            miner_addr,
+            reward: 100.0,
+        };
+        chain.generate_new_block();
+        chain
+    }
+    pub fn new_transaction(
+        &mut self, 
+        sender: String, 
+        receiver: String, 
+        amount: f32
+        )-> bool{
+            self.curr_trans.push(Transaction{
+                sender,
+                receiver,
+                amount,
+            });
+            true          
+    }
+    pub fn last_hash(&self)-> String{
+        let block = match self.chain.last(){
+            Some(Block) => block,
+            None => return String::from_utf8(vec! [48, 64]).unwrap(),
+        };
+        chain::hash(&block.header)
+    }
+    pub fn update_dificulty(&mut self, difficulty: u32) -> bool{
+        self.difficulty = difficulty;
+        true
+    }
+    pub fn update_reward(&mut self, reward: f32)-> bool{
+        let header = blockheader{
+            timestamp: Utc::now().timestamp_millis(),
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            merkle: String::new(),
+            dificulty: self.difficulty,
+        };
+        let reward_trans = Transaction{
+            sender: String::From("Root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward;
+        };
+        let mut block = Block{
+            header, 
+            count: 0,
+            transaction: Vec![],
+        };
+        block.transaction.push(reward_trans);
+        block.transaction.append(&mut self.curr_trans);
+        block.count = block.transaction.len() as u32;
+        block.header.merkle = Chain::get_merkle(block.transaction.clone());
+        Chain::proof_of_work(&mut block.header);
+
+        println!("{:#?}", &block);
+        self.chain.push(block);
+        true
+    }
+    fn get_merkle(curr_trans: Vec<Transaction>)-> String{
+        let mut merkle = Vec::new();
+
+        for t in curr_trans{
+            let hash = chain::hash(t);
+            merkle.push(hash);
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().clone().unwrap();
+            merkle.push(last);
+        }
+        while merkle.len()> 1{
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Chain::hash(&h1);
+            merkle.push(nh),
+        }
+        merkle.pop().unwrap()
+    }
+    pub fn proof_of_work(header: &mut Blockheader){
+        loop{
+            let hash = Chain::hash(header);
+            let slice = &hash[..header.difficulty as usize];
+            match slice.parse::<u32>(){
+                Ok(val) => {
+                    if val != 0{
+                        header.nonce +=1;
+                    }else{
+                        println!("Block hash: {}", hash);
+                        breack;
+                    }
+                }
+                Err(_) => {
+                    header.nonce +=1;
+                    continue;
+                }
+            };
+        }
+    }
+    pub fn hash<T: serde::Serialize>(Item: &T)-> String{
+        let input = serde_json::to_string(&item).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.update(input.as_bytes());
+        let res = hasher.finalize();
+        let vec_res = res.to_vec();
+
+        Chain::hex_to_string(vec_res.as_slice())
+    }
+    pub fn hex_to_string(vec_res: &[u8])-> String{
+        let mut s = String::new();
+        for b in vec_res{
+            write!(&mut s, "{:x}", b).expect("unable to write");
+        }
+        s
+    }
+}
---
+++ Rust_Blockchain/readme.md
@@ -0,0 +1,3 @@
+#Rust Blockchain 
+##base on tuto found:
+###https://youtu.be/U8GGZ4TqlQs
